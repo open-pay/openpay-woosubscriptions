dd7d12cdf232ef153e2949134eb52db6e98dda85
Jose Guadalupe Romero <jose.romero@openpay.mx>
2021-01-18 12:35:32 -0600

    Unificación del plugin para procesar pagos México/Colombia

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,23 @@
 jQuery(document).ready(function () {
+    var country = jQuery('#woocommerce_openpay_country').val();
+    console.log('admin.js', country);
+    showOrHideElements(country);
+
+    function showOrHideElements(country) {
+        if(country === 'MX'){
+            jQuery("#woocommerce_openpay_iva").closest("tr").hide();
+        }else if(country === 'CO'){
+            jQuery("#woocommerce_openpay_iva").closest("tr").show();
+        }
+    }
+
+    jQuery('#woocommerce_openpay_country').change(function () {
+        var country = jQuery(this).val();
+        console.log('woocommerce_openpay_country', country);        
+
+        showOrHideElements(country)
+    });
+
     if(jQuery("#woocommerce_openpay_sandbox").length){
         is_sandbox();
 
